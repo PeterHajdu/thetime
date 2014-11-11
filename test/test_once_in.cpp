9612be8066b26a370cc04caf60a463b6bbe713f1
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-11-11 08:37:26 +0100

    add mission ut / test file cleanup

--- test/test_once_in.cpp
+++ test/test_once_in.cpp
@@ -39,45 +39,13 @@ Describe( once_in )
   It( runs_the_given_code_block_after_interval_time )
   {
     AssertThat( did_run_after( interval ), Equals( true ) );
-    AssertThat( did_run_after( interval ), Equals( true ) );
   }
 
- // void tick_after( test::Clock::Time time )
- // {
- //   test_clock->sleep_for( time );
- //   frequency_meter->tick();
- // }
-
- // It( should_measure_the_frequency_of_ticks )
- // {
- //   tick_after( test::Clock::ticks_per_second );
- //   AssertThat( frequency_meter->per_second(), EqualsWithDelta( 1.0, 0.1 ) );
-
- //   const test::Clock::Time sixty_per_second_interval{ 16667u };
- //   tick_after( sixty_per_second_interval );
- //   AssertThat( frequency_meter->per_second(), EqualsWithDelta( 60.0, 0.1 ) );
- // }
-
- // void check_interval_after( test::Clock::Time time )
- // {
- //   tick_after( time );
- //   AssertThat( frequency_meter->interval(), Equals( time ) );
- // }
-
- // It( should_measure_interval_between_ticks )
- // {
- //   check_interval_after( test::Clock::Time( 100 ) );
- //   check_interval_after( test::Clock::Time( 101 ) );
- // }
-
- // It( works_with_thetime_clock )
- // {
- //   Clock real_clock;
- //   FrequencyMeter< Clock > fm( real_clock );
- //   fm.tick();
- //   fm.per_second();
- //   fm.interval();
- // }
+  It( resets_the_timer_after_each_execution )
+  {
+    AssertThat( did_run_after( interval ), Equals( true ) );
+    AssertThat( did_run_after( interval - 1 ), Equals( false ) );
+  }
 
   std::unique_ptr< test::Clock > test_clock;
   std::unique_ptr< OnceIn< test::Clock > > oncein;
